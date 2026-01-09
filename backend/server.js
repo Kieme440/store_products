84fe1e4205dd68a3427306dbb0fce15e159eb5e5
Kieme440 <Kieme440@gmail.com>
2026-01-09 19:11:26 +0100

    testing2

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 5001;
 
 app.use("inventory/lists", inventoryRoute);
+app.use("inventory/products", inventoryRoute);
 
 app.listen("PORT", ()=>{
     console.log("loaded successfully at port ${PORT}");
